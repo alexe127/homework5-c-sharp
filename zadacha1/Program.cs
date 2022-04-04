30c94256e4a3364e9444463676a850c728d54a6f
«Alex» <aser_e@mail.ru>
2022-04-05 00:17:05 +0900

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, 
+// b1 k1 и b2 и k2 заданы
+double b1 = 1;
+double k1 = 5;
+double b2 = 2;
+double k2 = 4;
+
+if (k1 == k2)
+    Console.WriteLine("Прямые параллельны");
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine("Координата точки пересечения двух прямых " + "(" + x + ","+ y +")");
+    
+}
