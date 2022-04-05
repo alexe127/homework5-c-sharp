728d2b48f61b53c20962bcb2bcb429ca509ce8e3
«Alex» <aser_e@mail.ru>
2022-04-06 00:20:48 +0900

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу масштабирования фигуры
+int x1=0; int y1=0;
+int x2=4; int y2=0;
+int x3=4; int y3=2;
+int x4=0; int y4=2;
+int k = 2;
+Console.WriteLine("x1 = "+ x1*k + " y1 = " + y1*k);
+Console.WriteLine("x2 = "+ x2*k + " y2 = " + y2*k);
+Console.WriteLine("x3 = "+ x3*k + " y3 = " + y3*k);
+Console.WriteLine("x4 = "+ x4*k + " y4 = " + y4*k);
