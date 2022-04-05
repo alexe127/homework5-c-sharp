f904e42c94ac389398643c0ab5fffdbd422de4ec
«Alex» <aser_e@mail.ru>
2022-04-05 22:57:34 +0900

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать числа Фибоначчи
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int a1 = 0;
+int a2 = 1;
+if (n > 0)
+    Console.Write(a1 + " " + a2 + " ");
+else
+    Console.Write("таких чисел Фибоначчи не существует");
+
+int temp = 0;
+int i = 0;
+while (i < n)
+{
+    Console.Write((a1 + a2) + " ");
+    temp = a2;
+    a2 = a2 + a1;
+    a1 = temp;
+    i++;    
+}
