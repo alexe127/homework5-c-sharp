8f15522c92b0053c3fdf507fb3e83c280cfea106
«Alex» <aser_e@mail.ru>
2022-04-13 22:36:56 +0900

    zadacha3

--- zadacha3/Program.cs
+++ zadacha3/Program.cs
@@ -1,10 +1,19 @@
 ﻿// Написать программу масштабирования фигуры
-int x1=0; int y1=0;
-int x2=4; int y2=0;
-int x3=4; int y3=2;
-int x4=0; int y4=2;
-int k = 2;
-Console.WriteLine("x1 = "+ x1*k + " y1 = " + y1*k);
-Console.WriteLine("x2 = "+ x2*k + " y2 = " + y2*k);
-Console.WriteLine("x3 = "+ x3*k + " y3 = " + y3*k);
-Console.WriteLine("x4 = "+ x4*k + " y4 = " + y4*k);
+Console.WriteLine("Введите колличество вершин фигуры: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите коэффициент масштабирования: ");
+int k = int.Parse(Console.ReadLine() ?? "");
+int [] coord = new int [num*2];
+
+for (int count = 0; count < num*2; count+=2)
+{
+    Console.WriteLine("Введите x: ");
+    coord[count] = int.Parse(Console.ReadLine()?? "");
+    Console.WriteLine("Введите y: ");
+    coord[count + 1] = int.Parse(Console.ReadLine()?? "");
+}
+    
+for (int count = 0; count < num*2; count+=2)
+{
+        Console.WriteLine($"x = {coord[count] * k} y = {coord[count + 1] * k}");
+}
